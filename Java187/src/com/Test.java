df004e32ae3daba4f6a4f2a2783e0177831b48a3
FirstComputer <FirstComputer@DESKTOP-44EFAV3>
2025-01-22 16:16:41 +0530

    Add Mutiplication Method

--- Java187/src/com/Test.java
+++ Java187/src/com/Test.java
@@ -3,13 +3,18 @@ package com;
 public class Test {
 	public void Addition() {
 		System.out.println("Thise is addition method");
-
+	}
 		public void Substract(){
 			System.out.println("Thise is Substraction method");
-
+		}
 			public void Multiplication(){
 				System.out.println("Thise is substraction method");
 		}
+			public void Division() {
+				System.out.println("Division Method");
+			}
 	}
+	
+
 
 
