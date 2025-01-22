950bbac9b201c2697c7d2a41f2cea6cd6e162d60
Tejashreekokane123 <tejashreekokane38@gmail.com>
2025-01-22 16:06:16 +0530

    Add sub method by tejashree

--- Java187/src/com/Test.java
+++ Java187/src/com/Test.java
@@ -2,7 +2,10 @@ package com;
 
 public class Test {
 	public void Addition() {
-			
+		System.out.println("Thise is addition method");
+
+		public void Substract(){
+			System.out.println("Thise is Substraction method");
 		}
 	}
 
